6633bb67250433c82455571f2498e5fb940c4265
Bijou <bijou.raj12@gmail.com>
2024-04-14 17:59:51 -0700

    Added additional initial methods to class

--- Tasks/MaxHeap.java
+++ Tasks/MaxHeap.java
@@ -24,5 +24,91 @@ public final class MaxHeap<T extends Comparable<? super T>> implements MaxHeapIn
         lastIndex = 0;
         initialized = true;
     }
+
+    public T getMax()
+    {
+        checkInitialization();
+        T root = null;
+        if (!isEmpty())
+            root = heap[1];
+        return root;
+    }
+
+    public boolean isEmpty()
+    {
+        return lastIndex < 1;
+    }
+
+    public int getSize()
+    {
+        return lastIndex;
+    }
+
+    public void clear()
+    {
+        checkInitialization();
+        while (lastIndex > -1)
+        {
+            heap[lastIndex] = null;
+            lastIndex--;
+
+        }
+        lastIndex = 0;
+    }
+
+    public void add(T newEntry)
+    {
+        checkInitialization();
+        int newIndex = lastIndex + 1;
+        int parentIndex = newIndex / 2;
+        while ((parentIndex > 0) && newEntry.compareto(heap[parentIndex]) > 0)
+        {
+            heap[newIndex] = heap[parentIndex];
+            newIndex = parentIndex;
+            parentIndex = newIndex / 2;
+        }
+        heap[newIndex] = newEntry;
+        lastIndex++;
+        ensureCapacity();
+    }
+
+    private void reheap(int rootIndex)
+    {
+        boolean done = false;
+        T orphan = heap[rootIndex];
+        int leftChildIndex = 2 * rootIndex;
+        while (!done && (leftChildIndex <= lastIndex))
+        {
+            int largerChildIndex = leftChildIndex;
+            int rightChildIndex = leftChildIndex + 1;
+            if ((rightChildIndex <= lastIndex) && heap[rightChildIndex].compareTo(heap[largerChildIndex]) > 0)
+            {
+                largerChildIndex = rightChildIndex;
+            }
+            if (orphan.compareTo(heap[largerChildIndex]) < 0)
+            {
+                heap[rootIndex] = heap[largerChildIndex];
+                rootIndex = largerChildIndex;
+                leftChildIndex = 2 * rootIndex;
+            }
+            else
+                done = true;
+        }
+        heap[rootIndex] = orphan;
+    }
+
+    public T removeMax()
+    {
+        checkInitialization();
+        T root = null;
+        if (!isEmpty())
+        {
+            root = heap[1];
+            heap[1] = heap[lastIndex];
+            lastIndex--;
+            reheap(1);
+        }
+        return root;
+    }
 }
 
